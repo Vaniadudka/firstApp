c5a0dfee235fdfc685cb32fddad9722facccf06d
Ваня <dudka.ivan01@gmail.com>
2022-10-31 19:27:01 +0200

    commit dz4(1).py)

---
+++ dz4(1).py
@@ -0,0 +1,17 @@
+class Human:
+    Brain = ""
+    Heart = ""
+    Legs = ""
+
+
+
+
+class Brain(Human):
+    print("I think")
+
+class Heart(Human):
+    print("I circulate blood")
+
+class Legs(Human):
+    print("I go")
+
