426708839cd12bfd218fc4f786e654d96ebdfa1c
Ваня <dudka.ivan01@gmail.com>
2022-11-02 10:09:59 +0200

    commit dz5

--- dz5.py
+++ dz5.py
@@ -1,3 +1,15 @@
 import colorama
 
 print(help(colorama))
+
+cl = colorama
+
+print(cl.__name__)
+print(type(cl))
+
+name = ""
+for method in dir(name):
+    print(method)
+
+print(hasattr(name, "index"))
+print(hasattr(name, "initialise"))
