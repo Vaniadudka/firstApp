a84f6c2d83e4a2a0e126caa13ec782580443102b
Ваня <dudka.ivan01@gmail.com>
2022-11-11 08:48:10 +0200

    commit dz7

--- dz7.py
+++ dz7.py
@@ -1,3 +1,13 @@
+myList = []
+Droid = "C3o", "R2D2", "BB8", "Bfd4", "Vds5", "BHKm9"
+print(Droid)
+
+for i in myList:
+    print(i)
+
+
+myIterator = iter(myList)
+
 class Droid:
     def __init__(self, name,model):
         self.name = name
@@ -7,21 +17,25 @@ class Droid:
 
 class Shop:
     def __init__(self):
-        self.droids = [Droid("C3po"), Droid("R2D2"), Droid("BB8")]
+        self.droids = [Droid("C3po"), Droid("R2D2"), Droid("BB8"), Droid("Bfd4"), Droid("Vds5"), Droid("BHKm9")]
         self.count = len(self.droids)
-
-    def addDroid(self, newDroid):
-            self.Droid.append(newDroid)
-
-    def delDroid(self, delDroid):
-        self.Droid.remove(delDroid)
-
-    def __init__(self):
-        self.i = 0
+        self.d = 0
 
     def __iter__(self):
-        self.i = 0
+        self.d = Droid
         return self
 
+    def __next__(self):
+        self.d += Droid
+        return self.d
+
+    def addDroid(self, newDroid):
+            self.d.append(newDroid)
+
+    def delDroid(self, delDroid):
+        self.d.remove(delDroid)
+
+
+
 ShowInfo = Shop
-ShowInfo = Droid
+
